8f938e3da35c9496e43467c76d0cb3e0e6349688
Yang Bo <yang.bo@gses.com>
2015-10-13 17:21:53 -0400

    added cpu usage and toggle window on top

--- js/main.js
+++ js/main.js
@@ -1,14 +1,60 @@
 var os = require("os");
-setInterval(refresh, 1000);
+var gui = require('nw.gui');
+var win = gui.Window.get();
+var onTop = false;
 
+var oldCPUInfo = null;
+var newCPUInfo = null;
 
 function refresh() {
+
+    //memory
     var totalMemory = os.totalmem();
     var freeMemory = os.freemem();
     var used = (totalMemory-freeMemory)/1024/1024/1024;
     var percent = freeMemory/totalMemory;
     document.getElementById("free").style.height = percent*200 +"px";
     document.getElementById("number").innerHTML = used.toFixed(2) +" GB";
+
+
+    //cpu, calculate the average CPU usage in the past iteration
+    newCPUInfo = os.cpus();
+
+    if(oldCPUInfo != null){
+        var oldTotalCPU = 0;
+        var oldIdleCPU = 0;
+        var newTotalCPU = 0;
+        var newIdleCPU = 0;
+
+        for(var i in oldCPUInfo){
+            for(var type in oldCPUInfo[i].times) {
+                oldTotalCPU += oldCPUInfo[i].times[type];
+            }
+            oldIdleCPU += oldCPUInfo[i].times.idle;
+        }
+
+        for(var i in newCPUInfo){
+            for(var type in newCPUInfo[i].times) {
+                newTotalCPU += newCPUInfo[i].times[type];
+            }
+            newIdleCPU += newCPUInfo[i].times.idle;
+        }
+
+
+        var idlePercent = (newIdleCPU-oldIdleCPU)/(newTotalCPU-oldTotalCPU);
+
+        var cpuPercent = 100*(1-idlePercent);
+
+        document.getElementById("cpu_usage").style.height = idlePercent*200 +"px";
+        document.getElementById("cpu_number").innerHTML = cpuPercent.toFixed(2) +" %";
+    }
+
+    oldCPUInfo = newCPUInfo;
+
+
+
+
+
 }
 
 
@@ -24,6 +70,19 @@ function close2(){
     win.close();
 }
 
+function toggleOnTop(){
+    onTop = !onTop;
+    win.setAlwaysOnTop(onTop);
+    if(onTop){
+        document.getElementById("ontop").src = "../img/ontop.svg";
+    }else{
+        document.getElementById("ontop").src = "../img/not-ontop.png";
+    }
+}
 
+window.onload = function (){
+    refresh();
+    setInterval(refresh, 1000);
+}
 
 
